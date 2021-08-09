82bbfaa3dfe762b918e3c272ec7ed0a226c92661
Sumna Imran <88527179+SumnaImran-20@users.noreply.github.com>
2021-08-09 19:35:46 +0500

    Add files via upload

---
+++ array.java
@@ -0,0 +1,21 @@
+class array
+{
+	public static void main(String[] args)
+	{
+		System.out.println("---Sum Of digits pesent inside an array---");
+	int [] numbers = {1,2,3,4,5,6};
+		System.out.println("Digits are:");
+		int sum =0;
+		for (int i=0; i < numbers.length ; i++)
+		{
+			System.out.println(numbers[i]);
+		}
+		
+		for (int i=0; i < numbers.length ; i++)
+		{
+			sum = sum +i;
+		}
+		System.out.println();
+		System.out.println("Sum is: " + sum);
+	}	
+}
